aaad5b9d70036dd2e31a8b104980c8dc1059ca5a
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-09-07 20:50:01 -0400

    warnings before saving

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,105 +1,124 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TimerTime {
-	mins: number;
-	seconds: number;
-	constructor(mins: number, seconds?: number) {
-		this.mins = mins;
-		this.seconds = seconds ? seconds : 0;
-	}
-	next(): TimerTime {
-		const x = new TimerTime(this.mins, this.seconds);
-		if (this.seconds === 0) {
-			x.mins -= 1;
-			x.seconds = 59;
-		} else {
-			x.seconds -= 1;
-		}
-		return x;
-	}
-	zero() {
-		return this.seconds === 0 && this.mins === 0;
-	}
+  mins: number;
+  seconds: number;
+  constructor(mins: number, seconds?: number) {
+    this.mins = mins;
+    this.seconds = seconds ? seconds : 0;
+  }
+  next(): TimerTime {
+    const x = new TimerTime(this.mins, this.seconds);
+    if (this.seconds === 0) {
+      x.mins -= 1;
+      x.seconds = 59;
+    } else {
+      x.seconds -= 1;
+    }
+    return x;
+  }
+  zero() {
+    return this.seconds === 0 && this.mins === 0;
+  }
 }
 
 type TimerProps = { mins: number; seconds?: number };
 type TimerState = { running: boolean; time: TimerTime };
 
 export default class Timer extends Component<TimerProps, TimerState> {
-	interval: any;
-	defaultTime: TimerTime;
-	constructor(props: TimerProps) {
-		super(props);
-		this.state = {
-			running: false,
-			time: new TimerTime(props.mins, props.seconds),
-		};
-		this.interval = null;
-		this.defaultTime = new TimerTime(
-			props.mins,
-			props.seconds ? props.seconds : 0
-		);
-	}
-	down = () => {
-		if (!this.state.time.zero()) {
-			this.setState({ time: this.state.time.next() });
-		} else {
-			this.stop();
-		}
-	};
-	start = () => {
-		this.interval = setInterval(() => {
-			this.down();
-		}, 1000);
-		this.setState({ running: true });
-	};
-	stop = () => {
-		clearInterval(this.interval);
-		this.setState({ running: false });
-	};
-	reset = () => {
-		this.setState({
-			time: new TimerTime(this.defaultTime.mins, this.defaultTime.seconds),
-		});
-	};
-	render() {
-		const body = (
-			<div>
-				<div className='text-2xl'> Timer </div>
-				<div className='text-2xl'>
-					{this.state.time.mins} m {this.state.time.seconds} s
-				</div>
-				<div className='flex'>
-					<button
-						className='flex-auto bg-green-400 hover:bg-green-500 p-3 m-1 rounded-lg text-white text-center transform hover:-translate-y-1'
-						onClick={this.state.running ? this.stop : this.start}
-					>
-						{this.state.running ? 'Stop' : 'Start'}
-					</button>
-					{!this.state.running && (
-						<button
-							className='flex-auto bg-blue-400 hover:bg-blue-500 p-3 m-1 rounded-lg text-white text-center transform hover:-translate-y-1'
-							onClick={this.reset}
-						>
-							Reset
-						</button>
-					)}
-				</div>
-			</div>
-		);
+  interval: any;
+  defaultTime: TimerTime;
+  constructor(props: TimerProps) {
+    super(props);
+    this.state = {
+      running: false,
+      time: new TimerTime(props.mins, props.seconds),
+    };
+    this.interval = null;
+    this.defaultTime = new TimerTime(
+      props.mins,
+      props.seconds ? props.seconds : 0
+    );
+  }
 
-		if (this.state.time.zero()) {
-			return (
-				<div className='bg-red-100 shadow-xl hover:shadow-2xl my-2 mx-3 md:mx-6 p-3 w-96 rounded-xl'>
-					{body}
-				</div>
-			);
-		} else {
-			return (
-				<div className='bg-gray-100 shadow-xl hover:shadow-2xl my-2 mx-3 md:mx-6 p-3 w-96 rounded-xl'>
-					{body}
-				</div>
-			);
-		}
-	}
+  componentDidMount() {
+    document.addEventListener("beforeunload", this.beforeunload);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("beforeunload", this.beforeunload);
+  }
+  down = () => {
+    if (!this.state.time.zero()) {
+      this.setState({ time: this.state.time.next() });
+    } else {
+      this.stop();
+    }
+  };
+  start = () => {
+    this.interval = setInterval(() => {
+      this.down();
+    }, 1000);
+    this.setState({ running: true });
+  };
+  stop = () => {
+    clearInterval(this.interval);
+    this.setState({ running: false });
+  };
+  reset = () => {
+    this.setState({
+      time: new TimerTime(this.defaultTime.mins, this.defaultTime.seconds),
+    });
+  };
+  started = () => {
+    return (
+      this.state.time.mins !== this.defaultTime.mins ||
+      this.state.time.seconds !== this.defaultTime.seconds
+    );
+  };
+  beforeunload = (e: Event) => {
+    if (!this.started()) {
+      e.preventDefault();
+      e.returnValue = true;
+    }
+  };
+  render() {
+    const body = (
+      <div>
+        <div className="text-2xl"> Timer </div>
+        <div className="text-2xl">
+          {this.state.time.mins} m {this.state.time.seconds} s
+        </div>
+        <div className="flex">
+          <button
+            className="flex-auto bg-green-400 hover:bg-green-500 p-3 m-1 rounded-lg text-white text-center transform hover:-translate-y-1"
+            onClick={this.state.running ? this.stop : this.start}
+          >
+            {this.state.running ? "Stop" : "Start"}
+          </button>
+          {!this.state.running && (
+            <button
+              className="flex-auto bg-blue-400 hover:bg-blue-500 p-3 m-1 rounded-lg text-white text-center transform hover:-translate-y-1"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+          )}
+        </div>
+      </div>
+    );
+
+    if (this.state.time.zero()) {
+      return (
+        <div className="bg-red-100 shadow-xl hover:shadow-2xl my-2 mx-3 md:mx-6 p-3 w-96 rounded-xl">
+          {body}
+        </div>
+      );
+    } else {
+      return (
+        <div className="bg-gray-100 shadow-xl hover:shadow-2xl my-2 mx-3 md:mx-6 p-3 w-96 rounded-xl">
+          {body}
+        </div>
+      );
+    }
+  }
 }
--- src/lib/questions.tsx
+++ src/lib/questions.tsx
@@ -1,41 +1,48 @@
-import React from 'react';
+import React from "react";
 
 export enum AnswerState {
-	undefined = 0,
-	correct = 1,
-	incorrect = 2,
+  undefined = 0,
+  correct = 1,
+  incorrect = 2,
 }
 
-export const getAnswerStateEl = (state: AnswerState) => {
-	if (state === AnswerState.correct) {
-		return (
-			<div className='m-2 p-1 bg-green-500 text-center text-white rounded-lg flex-auto flex'>
-				<div className='m-auto'>Correct</div>
-			</div>
-		);
-	} else if (state === AnswerState.incorrect) {
-		return (
-			<div className='m-2 p-1 bg-red-500 text-center text-white rounded-lg flex-auto flex'>
-				<div className='m-auto'>Incorrect</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className='m-2 p-1 bg-blue-800 text-center text-white rounded-lg flex-auto flex'>
-				<div className='m-auto'>Not Solved</div>
-			</div>
-		);
-	}
+export const getAnswerStateEl = (state: AnswerState, answer: any) => {
+  if (state === AnswerState.correct) {
+    return (
+      <div className="m-2 p-1 bg-green-500 text-center text-white rounded-lg flex-auto flex">
+        <div className="m-auto">Correct</div>
+      </div>
+    );
+  } else if (state === AnswerState.incorrect) {
+    return (
+      <div className="m-2 p-1 bg-red-500 text-center text-white rounded-lg flex-auto flex">
+        <div className="m-auto">Incorrect</div>
+      </div>
+    );
+  } else {
+    if (answer) {
+      return (
+        <div className="m-2 p-1 bg-yellow-500 text-center text-white rounded-lg flex-auto flex">
+          <div className="m-auto">Solved</div>
+        </div>
+      );
+    }
+    return (
+      <div className="m-2 p-1 bg-blue-800 text-center text-white rounded-lg flex-auto flex">
+        <div className="m-auto">Not Solved</div>
+      </div>
+    );
+  }
 };
 
 export const defaults = (number: number): [number[], any[], AnswerState[]] => {
-	const ar: number[] = [];
-	const a: (number | null | string)[] = [];
-	const b: AnswerState[] = [];
-	for (let i: number = 0; i < number; i++) {
-		ar.push(i);
-		a.push(null);
-		b.push(AnswerState.undefined);
-	}
-	return [ar, a, b];
+  const ar: number[] = [];
+  const a: (number | null | string)[] = [];
+  const b: AnswerState[] = [];
+  for (let i: number = 0; i < number; i++) {
+    ar.push(i);
+    a.push(null);
+    b.push(AnswerState.undefined);
+  }
+  return [ar, a, b];
 };
