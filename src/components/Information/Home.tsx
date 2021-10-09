b7fa061e64bb29cd64758fbcc9698c6485ed08e7
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-10-09 18:51:07 -0400

    citation modifications + youtube channel

--- src/components/Information/Home.tsx
+++ src/components/Information/Home.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../images/Logo.png';
-import Github from '../../images/Github.png';
-import Email from '../../images/Email.png';
-import Wordpress from '../../images/Wordpress.png';
+
 import Block, { Blue } from './Block';
 
 export default function Home() {
@@ -74,44 +72,6 @@ export default function Home() {
 						</ul>
 					</span>
 				</Block>
-				<div className='flex justify-center'>
-					<div className='bg-gray-100 dark:bg-gray-800 m-2 p-3 rounded-xl flex-1 max-w-2xl text-center'>
-						<h1 className='dark:text-white font-bold'> Contact </h1>
-						<div className='flex flex-row flex-wrap justify-evenly'>
-							<a
-								href='https://blog.maatester.com'
-								target='_blank'
-								rel='noreferrer'
-							>
-								<button className='transition duration-100 bg-gradient-to-r from-gray-100 to-gray-400 text-white rounded-xl p-4 m-2 transform hover:-translate-y-1 shadow-lg'>
-									<img
-										src={Wordpress}
-										alt='Wordpress Logo'
-										className='w-9 h-9'
-									/>
-								</button>
-							</a>
-							<a
-								href='https://github.com/junikimm717/MAA_Tester'
-								target='_blank'
-								rel='noreferrer'
-							>
-								<button className='transition duration-100 bg-gradient-to-r from-gray-600 to-black text-white rounded-xl p-4 m-2 transform hover:-translate-y-1 shadow-lg'>
-									<img src={Github} alt='Github Logo' className='w-9 h-9' />
-								</button>
-							</a>
-							<a
-								href='mailto:maatesterapp703.gmail.com'
-								target='_blank'
-								rel='noreferrer'
-							>
-								<button className='transition duration-100 bg-gradient-to-r from-gray-100 to-gray-400 text-white rounded-xl p-4 m-2 transform hover:-translate-y-1 shadow-lg'>
-									<img src={Email} alt='Email Logo' className='w-9 h-9' />
-								</button>
-							</a>
-						</div>
-					</div>
-				</div>
 			</div>
 		</>
 	);
