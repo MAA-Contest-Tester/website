e6e38d366313b39f6985828bcab11d4c564f5b7a
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-09-08 14:17:58 -0400

    nix flakes

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "MAA Tester Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ yarn ]; };
+
+        formatter = nixpkgs.legacyPackages."${system}".nixfmt;
+
+      });
+}
