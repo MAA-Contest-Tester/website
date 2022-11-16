6ae82fff7a29b93cb4946ec55710bff896b4a134
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-11-16 18:48:33 -0500

    loading component styled up

--- src/components/Lazy.tsx
+++ src/components/Lazy.tsx
@@ -1,5 +1,9 @@
 import React, { Suspense } from "react";
 
+function Loading() {
+  return <h2 className="font-bold text-center text-2xl m-5">Loading...</h2>;
+}
+
 export default function Lazy(props: { children?: any }) {
-  return <Suspense fallback={<h2>Loading...</h2>}>{props.children}</Suspense>;
+  return <Suspense fallback={<Loading />}>{props.children}</Suspense>;
 }
